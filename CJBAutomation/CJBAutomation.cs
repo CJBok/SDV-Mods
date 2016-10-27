ab426c8bf863cea1dff8d511fc6da70d0a18f591
huan <huancz@gmail.com>
2016-10-27 02:10:55 +0200

    bugfix: mushroom boxes emptied into chests were retaining their flower appearance

--- CJBAutomation/CJBAutomation.cs
+++ CJBAutomation/CJBAutomation.cs
@@ -645,6 +645,8 @@ namespace CJBAutomation {
                         {
                             obj.heldObject = null;
                             obj.readyForHarvest = false;
+                            obj.minutesUntilReady = -1;
+                            obj.showNextIndex = false;
                             break;
                         }
                     }
