ac356c265095694e7b7db6902a2f8202dfb44194
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-01 13:11:09 -0400

    add global assembly info

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("CJB mods")]
+[assembly: AssemblyCopyright("Copyright CJBok © 2016–2017")]
+[assembly: ComVisible(false)]
