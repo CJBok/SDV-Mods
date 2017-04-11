b1c194ad06c5c9c5252db4a8c48b367df2f8f8a4
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-04-11 18:32:23 -0400

    bump version for release

--- CJBItemSpawner/Properties/AssemblyInfo.cs
+++ CJBItemSpawner/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("CJBItemSpawner")]
 [assembly: Guid("ec7ea9a2-af3a-48ac-8048-024254359d5e")]
-[assembly: AssemblyVersion("1.7.1.0")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
+[assembly: AssemblyVersion("1.7.2.0")]
+[assembly: AssemblyFileVersion("1.7.2.0")]
