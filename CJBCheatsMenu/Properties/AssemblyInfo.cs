2253c9bba2e4f261c8b4c0794f0657d277a899d1
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-11-03 22:22:46 -0400

    bump version for release

--- CJBCheatsMenu/Properties/AssemblyInfo.cs
+++ CJBCheatsMenu/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("CJBCheatsMenu")]
-[assembly: AssemblyVersion("1.18.1")]
-[assembly: AssemblyFileVersion("1.18.1")]
+[assembly: AssemblyVersion("1.18.2")]
+[assembly: AssemblyFileVersion("1.18.2")]
